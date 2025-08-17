11597b25775eaa126788946406b779e552ca6ae6
kevinmatula <Matulakevin91@gmail.com>
2025-08-17 19:24:46 -0400

    feat: navbar for landing page

--- PlantKeepr/src/components/LandingTaskbar.tsx
+++ PlantKeepr/src/components/LandingTaskbar.tsx
@@ -1,11 +1,11 @@
 function LandingTaskBar() {
   return (
     <div className="landingtb">
-      <div className="tbwords">
+      <div className="tbwords-center">
         <button>Why PlantKeepr</button>
         <button>Behind PlantKeepr</button>
-        <button className="tbwords-side">Join</button>
       </div>
+      <button className="tbwords-side">Join</button>
     </div>
   );
 }
