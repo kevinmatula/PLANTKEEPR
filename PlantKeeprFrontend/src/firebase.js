02c69bba41f37775b41546f5d944b09864e1c271
kevinmatula <Matulakevin91@gmail.com>
2025-08-27 18:36:59 -0400

    chore: tweaking firebase again because stack overflow told me to

--- PlantKeeprFrontend/src/firebase.js
+++ PlantKeeprFrontend/src/firebase.js
@@ -4,12 +4,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCCfq26uoX-a5UHPNKNleokhLgZbn0Xrwk",
+  authDomain: "plantkeepr.firebaseapp.com",
+  projectId: "plantkeepr",
+  storageBucket: "plantkeepr.firebasestorage.app",
+  messagingSenderId: "1059712232880",
+  appId: "1:1059712232880:web:a36282fc2b6ab2cac3ba75",
+  measurementId: "G-577VGS7NPV",
 };
 
 // Initialize Firebase
